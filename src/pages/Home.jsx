8ad1635d71d2ef7a8db0dbc840205b524ecab85b
Ricky Alamsyah <rickyalamsyahp@gmail.com>
2022-03-14 08:12:58 +0700

    create navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
+import "../style/home.css";
+// import "../styles/post.css";
 const Home = () => {
   return (
-    <div>
-      <p>ricky</p>
-    </div>
+    <>
+      <div className="navbar">
+        <div className="nav">
+          <div className="left">
+            <p className="title">Ricky</p>
+          </div>
+        </div>
+      </div>
+      <div className="home">
+        <div className="cardHome">
+          <div className="left">
+            <p className="title">Ricky</p>
+            <p className="body">ALamsyah Permana</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
